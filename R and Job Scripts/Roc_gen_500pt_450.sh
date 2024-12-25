42f8fe4e34dbc0153aae29f6cf0e0951f01e178f
milsbeary <mileshkent@gmail.com>
2024-12-25 00:38:28 -0500

    Delete R and Job Scripts directory

--- R and Job Scripts/Roc_gen_500pt_450.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --time=12:00:00
-#SBATCH --account=def-wanghao
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=40
-#SBATCH --mem=0
-
-module restore my_modules
-Rscript ~/scratch/500pt_450ROC_forclust.R
